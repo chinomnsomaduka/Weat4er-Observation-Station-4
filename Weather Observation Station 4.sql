a8084308bb5eedad8df9cb53db1eb47561971917
Chinomnso <chinomnsocmaduka@gmail.com>
2021-09-10 13:21:45 -0400

    Add files via upload

---
+++ Weather Observation Station 4.sql
@@ -0,0 +1,6 @@
+/*
+Query the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the STATION table.
+*/
+SELECT
+    COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION;
